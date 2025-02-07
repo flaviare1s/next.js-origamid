13574317f6208923249a7c826113060abd04eeac
flaviare1s <flaviarster@gmail.com>
2025-02-07 15:37:25 -0300

    Criação de rotas dinâmicas

---
+++ src/app/cursos/page.tsx
@@ -0,0 +1,19 @@
+import { getCursos } from "@/api/cursos";
+import Link from "next/link";
+
+export default async function CursosPage() {
+  const cursos = await getCursos();
+
+  return (
+    <main>
+      <h1>Cursos</h1>
+      <ul>
+        {cursos.map((curso) => (
+          <li key={curso.id}>
+            <Link href={`/cursos/${curso.slug}`}>{curso.slug}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -4,9 +4,10 @@ export default function Menu() {
   return (
     <ul className="menu">
       <li><Link href="/">Home</Link></li>
-      <li><Link href="/sobre">Sobre</Link></li>
-      <li><Link href="/imc">IMC</Link></li>
+      {/* <li><Link href="/sobre">Sobre</Link></li>
+      <li><Link href="/imc">IMC</Link></li> */}
       <li><Link href="/produtos">Produtos</Link></li>
+      <li><Link href="/cursos">Cursos</Link></li>
     </ul>
   )
 }
